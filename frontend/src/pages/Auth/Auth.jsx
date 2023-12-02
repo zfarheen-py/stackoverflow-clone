0215a1cd34ddaf7a9b887378c565160a09945a71
Zainab Farheen <farheenzainab1104@gmail.com>
2023-12-03 01:35:28 +0530

    Frontend: add navbar, sidebar, home mainbar, ask qustion, display questions & auth

---
+++ frontend/src/pages/Auth/Auth.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import './Auth.css';
+import icon from '../../assets/icon.png';
+import AboutAuth from './AboutAuth';
+
+const Auth = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleSwitch = () => {
+    setIsSignUp(!isSignUp);
+  };
+  return (
+    <section className="auth-section">
+      {isSignUp && <AboutAuth />}
+      <div className="auth-container-2">
+        {!isSignUp && (
+          <img src={icon} alt="stack overflow" className="login-logo" />
+        )}
+        <form action="">
+          {isSignUp && (
+            <label htmlFor="name">
+              <h4>Display Name</h4>
+              <input type="text" id="name" name="name" />
+            </label>
+          )}
+          <label htmlFor="email">
+            <h4>Email</h4>
+            <input type="email" name="email" id="email" />
+          </label>
+          <label htmlFor="password">
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <h4>Password</h4>
+              <p style={{ color: '#007ac6', fontSize: '13px' }}>
+                forgot password?
+              </p>
+            </div>
+            <input type="password" name="password" id="password" />
+            {isSignUp && (
+              <p>
+                Passwords must contain atleast eight <br />
+                characters, including at least 1 letter and 1 <br />
+                number.
+              </p>
+            )}
+          </label>
+          {isSignUp && (
+            <label htmlFor="check">
+              <input type="checkbox" name="" id="check" />
+              <p style={{ fontSize: '13px' }}>
+                Opt-in to receive occasional, <br /> product updates, user
+                research invitations, <br /> company announcements, and digests.
+              </p>
+            </label>
+          )}
+          <button type="submit" className="auth-btn">
+            {isSignUp ? 'Sign up' : 'Log in'}
+          </button>
+          {isSignUp && (
+            <p style={{ color: '#666767', fontSize: '13px' }}>
+              By clicking 'Sign up', you agree to our
+              <span style={{ color: '#007ac6' }}>
+                {' '}
+                terms of <br /> service
+              </span>
+              ,<span style={{ color: '#007ac6' }}> privacy policy</span> and
+              <span style={{ color: '#007ac6' }}> cookie policy</span>
+            </p>
+          )}
+        </form>
+        <p>
+          {isSignUp ? 'Already have an account?' : "Don't have an account?"}
+          <button
+            type="button"
+            className="handle-switch-btn"
+            onClick={handleSwitch}
+          >
+            {isSignUp ? 'Log in' : 'Sign up'}
+          </button>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Auth;
---
+++ frontend/src/pages/Questions/QuestionsDetails.jsx
@@ -0,0 +1,149 @@
+import React from 'react'
+import { useParams, Link, useNavigate, useLocation } from 'react-router-dom'
+import upvote from '../../assets/sort-up.svg'
+import downvote from '../../assets/sort-down.svg'
+import './Questions.css'
+import Avatar from '../../components/Avatar/Avatar'
+import DisplayAnswer from './DisplayAnswer'
+import moment from 'moment'
+
+const QuestionsDetails = () => {
+    const { id } = useParams()
+
+      var questionsList = [{
+        _id: '1',
+        upVotes: 3,
+        downVotes: 2,
+        noOfAnswers: 2,
+        questionTitle: "What is a function?", 
+        questionBody: "It meant to be",
+        questionTags: ["java", "node js", "react js", "mongoose"],
+        userPosted: "Nithya", 
+        userId: 1,
+        time: "aug 1",
+        answer: [{
+          answerBody: "Answer",
+          userAnswered: 'shree',
+          answeredOn: "jan 2",
+          userId: 2,
+        }]
+      },{
+        _id: '2', 
+        upVotes: 3,
+        downVotes: 2,
+        noOfAnswers: 0,
+        questionTitle: "What is a function?", 
+        questionBody: "It meant to be",
+        questionTags: ["javascript", "R", "python"],
+        userPosted: "Nithya",
+        userId: 1,
+        time: "aug 1",
+        answer: [{
+          answerBody: "Answer",
+          userAnswered: 'shree',
+          answeredOn: "jan 2",
+          userId: 2,
+        }]
+      },{
+        _id: '3', 
+        upVotes: 3,
+        downVotes: 2,
+        noOfAnswers: 0, 
+        questionTitle: "What is a function?",
+        questionBody: "It meant to be", 
+        questionTags: ["javascript", "R", "python"],
+        userPosted: "Nithya", 
+        time: "aug 1",
+        userId: 1,
+        answer: [{
+          answerBody: "Answer",
+          userAnswered: 'shree',
+          answeredOn: "jan 2",
+          userId: 2,
+        }]
+      }] 
+  
+  return (
+    <div className='question-details-page'>
+        {
+            questionsList.data === null ?
+            <h1>Loading...</h1> :
+            <>
+                {
+                    questionsList.filter(question => question._id === id ).map(question => (
+                      <div key={question._id}>
+                        <section className='question-details-container'>
+                          <h1>{question.questionTitle}</h1>
+                          <div className='question-details-container-2'>
+                            <div className="question-votes">
+                              <img src={upvote} alt='' width='18' className='votes-icon' />
+                              <p>{question.upVotes - question.downVotes}</p>
+                              <img src={downvote} alt='' width='18' className='votes-icon' />
+                            </div>
+                            <div style={{width: "100%"}}>
+                              <p className='question-body'>{question.questionBody}</p>
+                              <div className="question-details-tags">
+                                {
+                                  question.questionTags.map((tag) => (
+                                    <p key={tag}>{tag}</p>
+                                  ))
+                                }
+                              </div>
+                              <div className="question-actions-user">
+                                <div>
+                                  <button type='button' >Share</button>
+                                  <button type='button' >Delete</button>
+
+                                  {/* {
+                                    User?.result?._id === question?.userId && (
+                                      <button type='button' >Delete</button>
+                                    )
+                                  } */}
+                                </div>
+                                <div>
+                                  <p>asked {moment(question.time).fromNow()}</p>
+                                  <Link to={`/Users/${question.userId}`} className='user-link' style={{color: '#0086d8'}}>
+                                    <Avatar backgroundColor="orange" px='8px' py='5px'>{question.userPosted.charAt(0).toUpperCase()}</Avatar>
+                                    <div>
+                                      {question.userPosted}
+                                    </div>
+                                  </Link>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </section>
+                         {
+                          question.noOfAnswers !== 0 && (
+                            <section>
+                              <h3>{question.noOfAnswers} Answers</h3> 
+                              <DisplayAnswer key={question._id} question={question} />
+                            </section>
+                          )
+                        }
+                        <section className='post-ans-container'>
+                          <h3>Your Answer</h3>
+                          <form >
+                            <textarea name='' id='' cols='30' rows='10' ></textarea> <br />
+                            <input type='submit' className='post-ans-btn' value='Post your Answer' />
+                          </form>
+                          <p>
+                            Browse other Question tagged
+                            {
+                              question.questionTags.map((tag) => (
+                                <Link to='/Tags' key={tag} className='ans-tags'> {tag} </Link>
+                              ))
+                            } or 
+                            <Link to='/AskQuestion' style={{textDecoration: "none", color: "#009dff"}}> ask your own question.</Link>
+                          </p>
+                        </section>
+                      </div>
+                    ))
+                }
+            </>
+        }
+    </div>
+  )
+}
+
+export default QuestionsDetails
