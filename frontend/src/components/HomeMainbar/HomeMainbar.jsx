2ce301e277a1cbc90728868b9b07f6dc1e71c5f6
Zainab Farheen <farheenzainab1104@gmail.com>
2023-12-06 01:42:37 +0530

    add left and right sidebars with main content for home page

---
+++ frontend/src/components/HomeMainbar/HomeMainbar.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './HomeMainbar.css';
+import QuestionList from './QuestionList';
+
+const HomeMainbar = () => {
+  var questionsList = [
+    {
+      _id: 1,
+      upVotes: 3,
+      downVotes: 2,
+      noOfAnswers: 2,
+      questionTitle: 'What is a function?',
+      questionBody: 'It meant to be',
+      questionTags: ['java', 'node js', 'react js', 'mongoose'],
+      userPosted: 'Nithya',
+      userId: 1,
+      time: 'aug 1',
+      answer: [
+        {
+          answerbody: 'Answer',
+          userAnswered: 'shree',
+          answeredOn: 'jan 2',
+          userId: 2,
+        },
+      ],
+    },
+    {
+      _id: 2,
+      upVotes: 3,
+      downVotes: 2,
+      noOfAnswers: 0,
+      questionTitle: 'What is a function?',
+      questionBody: 'It meant to be',
+      questionTags: ['javascript', 'R', 'python'],
+      userPosted: 'Nithya',
+      userId: 1,
+      time: 'aug 1',
+      answer: [
+        {
+          answerbody: 'Answer',
+          userAnswered: 'shree',
+          answeredOn: 'jan 2',
+          userId: 2,
+        },
+      ],
+    },
+    {
+      _id: 3,
+      upVotes: 3,
+      downVotes: 2,
+      noOfAnswers: 0,
+      questionTitle: 'What is a function?',
+      questionBody: 'It meant to be',
+      questionTags: ['javascript', 'R', 'python'],
+      userPosted: 'Nithya',
+      time: 'aug 1',
+      userId: 1,
+      answer: [
+        {
+          answerbody: 'Answer',
+          userAnswered: 'shree',
+          answeredOn: 'jan 2',
+          userId: 2,
+        },
+      ],
+    },
+  ];
+
+  const location = useLocation();
+
+  return (
+    <div className="main-bar">
+      <div className="main-bar-header">
+        {location.pathname === '/' ? (
+          <h1>Top Questions</h1>
+        ) : (
+          <h1>All Questions</h1>
+        )}
+        <Link to="/AskQuestion" className="ask-btn">
+          Ask Question
+        </Link>
+      </div>
+      <div>
+        {questionsList === null ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>
+            <p>{questionsList.length} questions</p>
+            <QuestionList questionsList={questionsList} />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomeMainbar;
