7cb36f535d6f95f11e75e4dc9f48371a612349e3
Zainab Farheen <farheenzainab1104@gmail.com>
2023-12-06 02:00:48 +0530

    create ask-question page using form to submit questions

--- frontend/src/AllRoutes.jsx
+++ frontend/src/AllRoutes.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Auth from './pages/Auth/Auth';
-// import Questions from './pages/Questions/Questions';
-// import AskQuestion from './pages/AskQuestion/AskQuestion';
-// import DisplayQuestion from './pages/Questions/DisplayQuestion';
+import Questions from './pages/Questions/Questions';
+import AskQuestion from './pages/AskQuestion/AskQuestion';
+import DisplayQuestion from './pages/Questions/DisplayQuestion';
 
 const AllRoutes = () => {
   return (
@@ -12,9 +12,9 @@ const AllRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Auth" element={<Auth />} />
-        {/* <Route path="/Questions" element={<Questions />} />
+        <Route path="/Questions" element={<Questions />} />
         <Route path="/AskQuestion" element={<AskQuestion />} />
-        <Route path="/Questions/:id" element={<DisplayQuestion />} /> */}
+        <Route path="/Questions/:id" element={<DisplayQuestion />} />
       </Routes>
     </div>
   );
