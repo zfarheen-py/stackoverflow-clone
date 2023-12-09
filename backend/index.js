450efa6806cdc7badd86ebdc17ddef89e646cad0
Zainab Farheen <farheenzainab1104@gmail.com>
2023-12-10 02:31:11 +0530

    add signup/login routes & controllers for user authentication

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import userRoutes from './routes/users.js';
+
 const app = express();
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
@@ -11,12 +13,18 @@ app.get('/', (req, res) => {
   res.send('This is a stack overflow clone API');
 });
 
+app.use('/user', userRoutes);
+
 const PORT = process.env.PORT || 5000;
 
-const CONNECTION_URl = "mongodb+srv://admin:admin123@stack-overflow-clone.w99uwx8.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URl =
+  'mongodb+srv://admin:admin123@stack-overflow-clone.w99uwx8.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect(CONNECTION_URl, { useNewUrlParser:true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, () => {
-  console.log(`Serving running at http://localhost:${PORT}`)
-}))
-.catch((err) => console.log(err.message))
+mongoose
+  .connect(CONNECTION_URl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Serving running at http://localhost:${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err.message));
