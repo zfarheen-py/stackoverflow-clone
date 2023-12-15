ee3888794477f138c31bbf75e3d441102dc7648c
Zainab Farheen <farheenzainab1104@gmail.com>
2023-12-16 01:51:17 +0530

    create a backend API route to handle user profile updates

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -19,3 +19,23 @@ export const getAllUsers = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const updateProfile = async (req, res) => {
+  const { id: _id } = req.params;
+  const { name, about, tags } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send('question unavailable...');
+  }
+
+  try {
+    const updatedProfile = await users.findByIdAndUpdate(
+      _id,
+      { $set: { name: name, about: about, tags: tags } },
+      { new: true }
+    );
+    res.status(200).json(updatedProfile);
+  } catch (error) {
+    res.status(405).json({ message: error.message });
+  }
+};
